14bbd710d3922d55e70d4bb163687c3c994c824b
Juan José Rojas <juanjoserojas99@gmail.com>
2022-09-02 14:43:01 -0500

    Proyecto

---
+++ AppRecimarket/src/main/java/cts/modelo/DAO/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cts.modelo.DAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author juan.rojas
+ */
+public class Conexion {
+    
+    Connection conectar = null;
+    
+    public Connection CONECTAR(){
+        
+        try {
+            
+            
+            Class.forName("");
+                    
+            conectar = DriverManager.getConnection("");
+            
+            System.out.println("Conexion exitosa");
+                    
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.out.println("Conexion exitosa");
+        }
+        return conectar;
+    }
+    
+}
