653134f393100712a6d922445a87ac4a5e8fdd47
Juan José Rojas <juanjoserojas99@gmail.com>
2022-09-02 22:16:38 -0500

    Archivo pom, controlador main
    Se agrego la dependencia de mariadb al archivo pom

--- AppRecimarket/src/main/java/recimarket/controlador/ControladorMain.java
+++ AppRecimarket/src/main/java/recimarket/controlador/ControladorMain.java
@@ -5,9 +5,7 @@ public class ControladorMain {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello world");
-
-        
+        System.out.println("Hello world");        
         
     }
     
