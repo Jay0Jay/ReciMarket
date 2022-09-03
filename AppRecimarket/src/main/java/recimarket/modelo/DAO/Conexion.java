70b8f202ab72a5a877c7723c3877f07a3a125c91
Juan José Rojas <juanjoserojas99@gmail.com>
2022-09-02 21:35:05 -0500

    Clean

--- AppRecimarket/src/main/java/recimarket/modelo/DAO/Conexion.java
+++ AppRecimarket/src/main/java/recimarket/modelo/DAO/Conexion.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package recimarket.modelo.DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author juan.rojas
- */
 public class Conexion {
     
     Connection conectar = null;
