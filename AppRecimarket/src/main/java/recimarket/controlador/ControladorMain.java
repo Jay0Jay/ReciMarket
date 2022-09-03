55f3d55c3ed5aa8d8b918ea5c5a20788c13e3476
Juan José Rojas <juanjoserojas99@gmail.com>
2022-09-02 21:48:53 -0500

    Update ControladorMain.java

--- AppRecimarket/src/main/java/recimarket/controlador/ControladorMain.java
+++ AppRecimarket/src/main/java/recimarket/controlador/ControladorMain.java
@@ -3,4 +3,12 @@ package recimarket.controlador;
 
 public class ControladorMain {
     
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world");
+
+        
+        
+    }
+    
 }
